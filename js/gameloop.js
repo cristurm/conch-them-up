022ce821d7fb9d0053fd671ada22a57dab628809
Cristina Sturm <cristurm@hotmail.com>
2014-03-28 18:54:51 -0300

    Loop Rearrangements and Game Over 10%
    Mainly organisation changes and beginning to implement the Game Over
    screen/interactions.

--- js/gameloop.js
+++ js/gameloop.js
@@ -44,6 +44,17 @@ var GameLoop = function () {
 
 			GC.uiUpdateScore(this.score);
 		},
+
+		theLoop: function () {
+			var gameLoop = this;		
+			
+			this.update();
+			this.draw();
+			
+			requestAnimationFrame(function () {
+				gameLoop.theLoop();
+			});
+		},
 		
 		scoreUp: function (_plusScore) {
 			this.score += _plusScore;
@@ -56,7 +67,12 @@ var GameLoop = function () {
 
 		scoreDown: function (_minusScore) {
 			this.score -= _minusScore;
-			this.score = this.score < 0 ? 0 : this.score;
+
+			if (this.score < 0) {
+				this.gameState = "gameOver";
+				this.score = 0;
+			}
+			//this.score = this.score < 0 ? 0 : this.score;
 			
 			this.scoreBarWidth = (this.scoreBarBGWidth / this.scoreGoal) * this.score;
 			
@@ -76,122 +92,162 @@ var GameLoop = function () {
 			var enemyIndex = this.enemies.indexOf(_enemy);
 			this.enemies.splice(enemyIndex, 1);
 		},
-	
-		theLoop: function () {
-			var gameLoop = this;		
-			
-			this.update();
-			this.draw();
-			
-			requestAnimationFrame(function () {
-				gameLoop.theLoop();
-			});
+
+		/* UPDATE METHODS */
+		initialStateUpdate: function () {
+			if (KB.isKeyDown("space") == true) {
+				this.gameState = "playing";
+				GC.clear(GC.uiContext);
+				GC.uiUpdateScore(this.score);
+			}
 		},
-		
-		update: function () {
-			if (this.gameState == "initial") {
-				if (KB.isKeyDown("space") == true) {
-					this.gameState = "playing";
-					GC.clear(GC.uiContext);
-					GC.uiUpdateScore(this.score);
-				}
 
-			} else if (this.gameState == "playing") {
-				this.mainChar.update();
+		playingStateUpdate: function () {
+			this.mainChar.update();
 			
-				// Summon Enemies Randomly
-				this.date = new Date();
-				this.currentTime = this.date.getTime();			
-				this.summonDifference = this.currentTime - this.pastTime;
-				
-				if (this.timerFlag) {
-					this.pastTime = this.date.getTime();
-					this.timerFlag = false;
-				} else {
-					if (this.summonDifference > 1000) {
-						this.summonEnemy();
-						
-						this.timerFlag = true;
-					}
-				}
-				
-				// Update Enemies
-				this.auxIndex = 0;
-				for (this.auxIndex = 0; this.auxIndex < this.enemies.length; this.auxIndex += 1) {
-					if(this.enemies[this.auxIndex]) {
-						var enemy = this.enemies[this.auxIndex];
-						
-						enemy.update();
-					}
-				}
-
-				// Update skills
-				this.auxIndex = 0;
-				for (this.auxIndex = 0; this.auxIndex < this.skills.length; this.auxIndex += 1) {
-					if(this.skills[this.auxIndex]) {
-						var skill = this.skills[this.auxIndex];
+			// Summon Enemies Randomly
+			this.date = new Date();
+			this.currentTime = this.date.getTime();			
+			this.summonDifference = this.currentTime - this.pastTime;
+			
+			if (this.timerFlag) {
+				this.pastTime = this.date.getTime();
+				this.timerFlag = false;
+			} else {
+				if (this.summonDifference > 1000) {
+					this.summonEnemy();
 					
-						skill.update();
-						
-						if (skill.posX > GC.width){
-							this.skills.splice(this.auxIndex, 1);
-						}
-					}
+					this.timerFlag = true;
 				}
 			}
 			
+			// Update Enemies
+			this.auxIndex = 0;
+			for (this.auxIndex = 0; this.auxIndex < this.enemies.length; this.auxIndex += 1) {
+				if(this.enemies[this.auxIndex]) {
+					var enemy = this.enemies[this.auxIndex];
+					
+					enemy.update();
+				}
+			}
+
+			// Update skills
+			this.auxIndex = 0;
+			for (this.auxIndex = 0; this.auxIndex < this.skills.length; this.auxIndex += 1) {
+				if(this.skills[this.auxIndex]) {
+					var skill = this.skills[this.auxIndex];
+				
+					skill.update();
+					
+					if (skill.posX > GC.width){
+						this.skills.splice(this.auxIndex, 1);
+					}
+				}
+			}
+		},
+
+		gameOverStateUpdate: function () {
+			console.log("K.O. !!!", this.gameState);
+		},
+		
+		update: function () {
+			switch (this.gameState) {
+				case "initial" :
+					this.initialStateUpdate();
+					break;	
+
+				case "playing" :
+					this.playingStateUpdate();
+					break;
+
+				case "gameOver" :
+					this.gameOverStateUpdate();
+					break;
+			}
+
 			// Pause / Unpause			
 			/*
 			prevEscKeyDown is used to detect if the esc key was previously pressed (hold),
 			we must treat it so the game on pause/unpause like crazy due to processing speed 
 			*/
-			if (KB.isKeyDown("esc") == true && this.prevEscKeyDown !== true) {
-				this.gameState = this.gameState == "playing" ? "paused" : "playing";
-				GC.clear(GC.uiContext);
-				GC.uiUpdateScore(this.score);
-			}
+			if (this.gameState == "playing" || this.gameState == "paused") {
+				if (KB.isKeyDown("esc") == true && this.prevEscKeyDown !== true) {
+					this.gameState = this.gameState == "playing" ? "paused" : "playing";
+					GC.clear(GC.uiContext);
+					GC.uiUpdateScore(this.score);
+				}
 
-			if (this.prevEscKeyDown !== KB.isKeyDown("esc")) {
-				this.prevEscKeyDown = KB.isKeyDown("esc");
+				if (this.prevEscKeyDown !== KB.isKeyDown("esc")) {
+					this.prevEscKeyDown = KB.isKeyDown("esc");
+				}
 			}
 		},
 		
+		/* DRAW METHODS */
+		initialStateDraw: function () {
+			GC.clear(GC.uiContext);
+			
+			// Draw Background Pattern
+			GC.gameDrawRectangle(this.bgPattern, 0, 0, GC.width, GC.height);
+
+			GC.uiWriteText("38px 'Press Start 2P'", 'white', 'Conch Them Up!', GC.width * 0.5, GC.height * 0.5 - 10, 'center');
+			GC.uiWriteText("bold 20px Arial", 'white', 'Press SPACEBAR do begin', GC.width * 0.5, GC.height * 0.5 + 20, 'center');
+		},
+
+		playingStateDraw: function () {
+			// Clear Game Canvas
+			GC.clear(GC.gameContext);
+			
+			// Draw Background Pattern
+			GC.gameDrawRectangle(this.bgPattern, 0, 0, GC.width, GC.height);
+
+			// Draw Main Character
+			this.mainChar.draw();
+			
+			// Draw Enemies
+			this.auxIndex = 0;
+			for (this.auxIndex = 0; this.auxIndex < this.enemies.length; this.auxIndex += 1) {
+				if(this.enemies[this.auxIndex]) {
+					this.enemies[this.auxIndex].draw();
+				}
+			}
+			
+			// Draw Skills
+			this.auxIndex = 0;
+			for (this.auxIndex = 0; this.auxIndex < this.skills.length; this.auxIndex += 1) {
+				if(this.skills[this.auxIndex]) {
+					this.skills[this.auxIndex].draw();
+				}
+			}
+		},
+
+		gameOverStateDraw: function () {
+			GC.clear(GC.uiContext);
+
+			// Draw Background Pattern
+			GC.gameDrawRectangle(this.bgPattern, 0, 0, GC.width, GC.height);
+
+			GC.uiWriteText("38px 'Press Start 2P'", 'black', 'GAME OVER', GC.width * 0.5, GC.height * 0.5 - 10, 'center');
+			GC.uiWriteText("bold 20px Arial", 'black', 'Press SPACEBAR do restart', GC.width * 0.5, GC.height * 0.5 + 20, 'center');
+		},
+
 		draw: function () {
-			if (this.gameState == "initial") {
-				GC.clear(GC.uiContext);
+			switch (this.gameState) {
+				case "initial" :
+					this.initialStateDraw();
+					break;
 
-				// Draw Background Pattern
-				GC.gameDrawRectangle(this.bgPattern, 0, 0, GC.width, GC.height);
+				case "playing" : 
+					this.playingStateDraw();
+					break;
 
-				GC.uiWriteText("38px 'Press Start 2P'", 'white', 'Conch Them Up!', GC.width * 0.5, GC.height * 0.5 - 10, 'center');
-				GC.uiWriteText("bold 20px Arial", 'white', 'Press SPACEBAR do begin', GC.width * 0.5, GC.height * 0.5 + 20, 'center');
-			} else if (this.gameState == "playing") {
-				// Clear Game Canvas
-				GC.clear(GC.gameContext);
-				
-				// Draw Background Pattern
-				GC.gameDrawRectangle(this.bgPattern, 0, 0, GC.width, GC.height);
+				case "paused" :
+					GC.uiWriteText("38px 'Press Start 2P'", 'white', 'PAUSED', GC.width * 0.5, GC.height * 0.5, 'center');
+					break;
 
-				// Draw Main Character
-				this.mainChar.draw();
-				
-				// Draw Enemies
-				this.auxIndex = 0;
-				for (this.auxIndex = 0; this.auxIndex < this.enemies.length; this.auxIndex += 1) {
-					if(this.enemies[this.auxIndex]) {
-						this.enemies[this.auxIndex].draw();
-					}
-				}
-				
-				// Draw Skills
-				this.auxIndex = 0;
-				for (this.auxIndex = 0; this.auxIndex < this.skills.length; this.auxIndex += 1) {
-					if(this.skills[this.auxIndex]) {
-						this.skills[this.auxIndex].draw();
-					}
-				}
-			} else if (this.gameState == "paused") {
-				GC.uiWriteText("38px 'Press Start 2P'", 'white', 'PAUSED', GC.width * 0.5, GC.height * 0.5, 'center');
+				case "gameOver" :
+					this.gameOverStateDraw();
+
 			}
 		}
 	}
